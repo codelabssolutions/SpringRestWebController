69809c4f7623013f91153d9816ecc3242add5352
hp <hp@192.168.0.8>
2018-08-04 14:31:39 +0530

    added domain object

---
+++ src/main/java/com/hotfix/common/responses/domainobject/ProductResponse.java
@@ -0,0 +1,42 @@
+package com.hotfix.common.responses.domainobject;
+
+public class ProductResponse {
+	private String id;
+	private String name;
+	private String description;
+	private String type;
+	
+	private String unit;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getUnit() {
+		return unit;
+	}
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	
+	
+}
---
+++ src/main/java/com/hotfix/common/responses/domainobject/ProductResponseList.java
@@ -0,0 +1,19 @@
+package com.hotfix.common.responses.domainobject;
+
+import java.util.List;
+
+public class ProductResponseList {
+	
+	List<ProductResponse> productList;
+
+	public List<ProductResponse> getProductList() {
+		return productList;
+	}
+
+	public void setProductList(List<ProductResponse> productList) {
+		this.productList = productList;
+	}
+	
+	
+
+}
